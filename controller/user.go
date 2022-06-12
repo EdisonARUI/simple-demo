fe4c36772e741af32f114e617f7be4eb686013c7
Arui <93503087+EdisonARUI@users.noreply.github.com>
2022-06-12 16:08:19 +0800

    完成Login接口构建与测试 (#3)
    完成Login接口构建与测试+完成Register接口构建与测试, 修改service中GetUserByName函数

--- controller/user.go
+++ controller/user.go
@@ -1,9 +1,12 @@
 package controller
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"sync/atomic"
+	"simple-demo/helper"
+	"simple-demo/model"
+	"simple-demo/service"
 )
 
 // usersLoginInfo use map to store user info, and key is username+password for demo
@@ -19,12 +22,11 @@ var usersLoginInfo = map[string]User{
 	},
 }
 
-var userIdSequence = int64(1)
-
 type UserLoginResponse struct {
 	Response
-	UserId int64  `json:"user_id,omitempty"`
-	Token  string `json:"token"`
+	UserId   int64  `json:"user_id,omitempty"`
+	Token    string `json:"token"`
+	Username string `json:"username"`
 }
 
 type UserResponse struct {
@@ -32,30 +34,31 @@ type UserResponse struct {
 	User User `json:"user"`
 }
 
+type UserRegisterService struct {
+	ctx context.Context
+}
+
 func Register(c *gin.Context) {
 	username := c.Query("username")
 	password := c.Query("password")
 
-	//userInfo := model.User{UserName: username, Password: password}
-	//service.CreateUser(&userInfo)
+	userInfo := model.User{UserName: username, Password: password}
+	password = helper.GetMd5(password)
+	token, _ := helper.GenerateToken(userInfo.UserName, userInfo.Password)
 
-	token := username + password
-
-	if _, exist := usersLoginInfo[token]; exist {
+	_, err := service.GetUserByName(userInfo.UserName)
+	if err == nil {
 		c.JSON(http.StatusOK, UserLoginResponse{
 			Response: Response{StatusCode: 1, StatusMsg: "User already exist"},
 		})
 	} else {
-		atomic.AddInt64(&userIdSequence, 1)
-		newUser := User{
-			Id:   userIdSequence,
-			Name: username,
-		}
-		usersLoginInfo[token] = newUser
+		userid, _ := service.CreateUser(&userInfo)
+
 		c.JSON(http.StatusOK, UserLoginResponse{
 			Response: Response{StatusCode: 0},
-			UserId:   userIdSequence,
-			Token:    username + password,
+			UserId:   userid,
+			Token:    token,
+			Username: username,
 		})
 	}
 }
@@ -64,16 +67,19 @@ func Login(c *gin.Context) {
 	username := c.Query("username")
 	password := c.Query("password")
 
-	//userInfo := model.User{UserName: username, Password: password}
-	//service.UserLogin(userInfo)
+	userInfo := model.User{UserName: username, Password: password}
+	userid, err := service.UserLogin(&userInfo)
 
-	token := username + password
 
-	if user, exist := usersLoginInfo[token]; exist {
+	password = helper.GetMd5(password)
+	token, _ := helper.GenerateToken(userInfo.UserName, userInfo.Password)
+
+	if err == nil {
 		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: Response{StatusCode: 0},
-			UserId:   user.Id,
+			Response: Response{StatusCode: 0, StatusMsg: "User exist"},
+			UserId:   userid,
 			Token:    token,
+			Username: username,
 		})
 	} else {
 		c.JSON(http.StatusOK, UserLoginResponse{
